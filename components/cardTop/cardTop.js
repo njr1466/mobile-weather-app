e751d795ed84949d64a97fad668b2b592ad151bc
Nilson Júnior <njr@gmail.com>
2023-05-02 15:16:24 -0300

    refactoring code

--- components/cardTop/cardTop.js
+++ components/cardTop/cardTop.js
@@ -16,17 +16,20 @@ const CardTop = (props) => {
       <View style={{ width: "100%", fekx: 1, flexDirection: "row" }}>
         <View style={{ width: "30%" }}><Text style={styles.description1}>
           <Entypo name="water" size={17} color="#ffffff" />
+          <Text style={styles.description}>  </Text>
           <Text style={styles.description}>{props.results.humidity}%</Text>
         </Text>
         </View>
         <View style={{ width: "30%" }}><Text style={styles.description2}>
-          <MaterialCommunityIcons name="weather-hail" size={22} color="#ffffff" />
+          <MaterialCommunityIcons name="weather-hail" size={17} color="#ffffff" />
+          <Text style={styles.description}> </Text>
           <Text style={styles.description}>{props.today.rain_probability}%</Text>
         </Text>
         </View>
         <View style={{ width: "40%" }}><Text style={styles.description2}>
-        <Feather name="wind" size={20} color="#ffffff" />
-      <Text style={styles.description}>{props.results.wind_speedy}</Text> 
+          <Feather name="wind" size={17} color="#ffffff" />
+          <Text style={styles.description}> </Text>
+          <Text style={styles.description}>{props.results.wind_speedy}</Text>
         </Text>
         </View>
       </View>
