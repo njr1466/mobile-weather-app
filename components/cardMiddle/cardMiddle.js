4690c59bc31e8ec1b533c61627510c9ac6931fca
Nilson Júnior <njr@gmail.com>
2023-05-02 15:42:55 -0300

    refactoring code

--- components/cardMiddle/cardMiddle.js
+++ components/cardMiddle/cardMiddle.js
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import styles from './styles';
 import MiniCard from './miniCard';
 
+
 const CardMiddle = (props) => {
 
   var color = props.color;
@@ -29,30 +30,3 @@ const CardMiddle = (props) => {
 
 
 export default CardMiddle;
-
-// import React, { Component } from 'react'
-// import { View, Text, StyleSheet } from 'react-native'
-
-// const App = (props) => {
-//    return (
-//       <View style = {styles.container}>
-//         <View style={{width:"45%",backgroundColor:'blue'}}><Text style={{textAlign:"left",paddingLeft:30}}>OLÄ</Text></View>
-
-//         <View style={{width:"45%",backgroundColor:'green'}}><Text style={{textAlign:"right",paddingRight:30}}>OLÄ</Text></View>
-//       </View>
-//    )
-// }
-// export default App;
-
-// const styles = StyleSheet.create ({
-//    container: {
-//       flexDirection: 'row',
-//       justifyContent:"space-between",
-//       marginTop:10,
-//       height: 50
-//    },
-//    item:{
-//      width:120,
-//      height:120
-//    }
-// })
